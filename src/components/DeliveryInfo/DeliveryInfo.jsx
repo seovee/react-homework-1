2f05fa560efa6870afecae606a1954133ce135cf
seovee <18073169+seovee@users.noreply.github.com>
2023-02-26 15:50:20 +0900

    deliveryInfo CSS 완성

--- src/components/DeliveryInfo/DeliveryInfo.jsx
+++ src/components/DeliveryInfo/DeliveryInfo.jsx
@@ -3,9 +3,16 @@ import Styles from './DeliveryInfo.module.css';
 
 export default function DeliveryInfo() {
   return (
-    <div>
-      <span>택배배송 | 3,000원(주문시 결제)</span>
-      <span>10개마다 부과</span>
-    </div>
+    <section className={Styles.DeliveryInfo}>
+      <div className={Styles.DeliveryInfoContainer}>
+        <div className={Styles.DeliveryInfo1}>
+          <span>택배배송</span>
+          <span>|</span>
+          <span>3,000원</span>
+          <span>(주문시 결제)</span>
+        </div>
+        <span className={Styles.DeliveryInfo2}>10개마다 부과</span>
+      </div>
+    </section>
   );
 }
