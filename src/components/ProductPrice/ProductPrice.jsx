a9d0b43f53a446dbf89bd71c747276958d90135c
seovee <18073169+seovee@users.noreply.github.com>
2023-02-26 17:25:25 +0900

    전역 style.css 수정

--- src/components/ProductPrice/ProductPrice.jsx
+++ src/components/ProductPrice/ProductPrice.jsx
@@ -8,7 +8,7 @@ export default function ProductPrice() {
       <div className={Styles.ProductPriceContainer}>
         <div className={Styles.ProductPriceTitle}>
           <span>총 상품 금액</span>
-          <img src={PriceQuestion} alt="" />
+          <img src={PriceQuestion} alt="'총 상품 금액'에 대한 정보입니다" />
         </div>
         <div className={Styles.ProductPriceContent}>
           <span>총 수량 1개 |</span>
